6762f790253f69647d6d79cd80d4c2e32c4b33b0
Santiago <santiagopoffo@hotmail.com>
2021-05-31 09:19:40 -0300

    finalizando JS

--- code.js
+++ code.js
@@ -1,28 +1,47 @@
 document.getElementById('cep').addEventListener('focusout', pesquisarcep)
 
+const cepValido = (cep) => cep.length == 8 && /^[0-9]+$/.test(cep)
+
+
 async function pesquisarcep() {
+    limparFormulario()
+
     let cep = document.getElementById('cep').value
     let url = `https://viacep.com.br/ws/${cep}/json/`
-    let dados = await fetch(url)
-    let endereco = await dados.json()
-    
-    if(endereco.hasOwnProperty('erro')) {
-        document.getElementById('endereco').value = 'CEP não encontrado!'
+    if(cepValido(cep)) {
 
+        let dados = await fetch(url)
+        let endereco = await dados.json()
+        
+        if(endereco.hasOwnProperty('erro')) {
+            document.getElementById('endereco').value = 'CEP não encontrado!'
+    
+        } else {
+            preencher(endereco)
+        }
+      
+        console.log(endereco)
     } else {
-        preencher(endereco)
+        document.getElementById('endereco').value = 'CEP incorreto!'
     }
-  
-    console.log(endereco)
+ 
     
 }
 
+function limparFormulario(endereco) {
+    document.getElementById('endereco').value = ''
+    document.getElementById('bairro').value = ''
+    document.getElementById('cidade').value = ''
+    document.getElementById('estado').value = ''
+
+}
+
 function preencher(endereco) {
+    
     document.getElementById('endereco').value = endereco.logradouro
     document.getElementById('bairro').value = endereco.bairro
     document.getElementById('cidade').value = endereco.localidade
     document.getElementById('estado').value = endereco.uf
 
-   
 }
 
